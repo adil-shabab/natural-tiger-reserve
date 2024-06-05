05b8569e193f00865b1b63d6742c3597be0bd7f8
adil-shabab <adilshabab2004@gmail.com>
2024-06-05 15:29:48 +0530

    uttar pradesh page is completed without responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,61 @@
+
+
+const bhadra_click = document.querySelectorAll('.bhadra_click');
+const nagarahole_click = document.querySelectorAll('.nagarahole_click');
+const kali_click = document.querySelectorAll('.kali_click');
+const biligiriranganatha_click = document.querySelectorAll('.biligiriranganatha_click');
+const bandipur_click = document.querySelectorAll('.bandipur_click');
+
+if (bhadra_click.length > 0) {
+    bhadra_click.forEach((item) => {
+        item.addEventListener('click', () => {
+            console.log(item)
+            document.getElementById('bhadra').scrollIntoView({ behavior: 'smooth' });
+        });
+    });
+}
+if (nagarahole_click.length > 0) {
+    nagarahole_click.forEach((item) => {
+        item.addEventListener('click', () => {
+            console.log(item)
+            document.getElementById('nagarahole').scrollIntoView({ behavior: 'smooth' });
+        });
+    });
+}
+if (kali_click.length > 0) {
+    kali_click.forEach((item) => {
+        item.addEventListener('click', () => {
+            console.log(item)
+            document.getElementById('kali').scrollIntoView({ behavior: 'smooth' });
+        });
+    });
+}
+if (biligiriranganatha_click.length > 0) {
+    biligiriranganatha_click.forEach((item) => {
+        item.addEventListener('click', () => {
+            console.log(item)
+            document.getElementById('biligiriranganatha').scrollIntoView({ behavior: 'smooth' });
+        });
+    });
+}
+if (bandipur_click.length > 0) {
+    bandipur_click.forEach((item) => {
+        item.addEventListener('click', () => {
+            console.log(item)
+            document.getElementById('bandipur').scrollIntoView({ behavior: 'smooth' });
+        });
+    });
+}
+
+
+
+
+
 const tooltips = document.querySelectorAll(".tooltip");
 const mapContainer = document.querySelector(".map-container");
 
 let currentTooltip = null; // Store the currently active tooltip (initially null)
+
 
 mapContainer.addEventListener("mousemove", (event) => {
     const target = event.target;
@@ -50,3 +104,4 @@ mapContainer.addEventListener("click", (event) => {
     // Redirect to state.html with the dynamic state value
     window.location.href = `${state}.html`;
 });
+
